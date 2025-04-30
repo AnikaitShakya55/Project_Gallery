ebbcdcf7499e2ad64aaa898fe3287c7f08322b34
Anikait Shakya <anikait.shakya@easexpense.com>
2025-04-30 08:45:55 +0530

    some icon changes

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -9,6 +9,7 @@ import cssIcon from "../Assests/css-3.png";
 import javaIcon from "../Assests/js.png";
 import nextIcon from "../Assests/nextjs.png";
 import bootStrapIcon from "../Assests/bootStrap.png";
+import nodeIcon from "../Assests/nodejs.png";
 import tailwindIcon from "../Assests/tailwind.png";
 import muiIcon from "../Assests/muiLogo.png";
 import envIcon from "../Assests/envLogo.png";
@@ -25,12 +26,13 @@ const AboutMe = () => {
         <h3 className="text-lg font-bold my-8">
           <Typewriter
             words={[
-              "Hi! I'm a React.js Front-End Wizard.",
-              "Hello! I'm a Creative UI/UX Designer.",
-              "Currently mastering Backend Development Skills.",
-              "Passionate about building interactive web experiences.",
-              "Designing user-friendly interfaces that captivate.",
-              "Exploring the world of backend technologies.",
+              "Hi! I'm an experienced Full Stack Engineer.",
+              "I have in-depth knowledge of React.js.",
+              "Hello! I'm a creative UI/UX Developer.",
+              "Currently sharpening my backend development skills.",
+              "Passionate about crafting dynamic web experiences.",
+              "Designing intuitive, user-centric interfaces that inspire.",
+              "Diving deeper into the world of backend technologies.",
             ]}
             loop={30}
             cursor
@@ -42,13 +44,13 @@ const AboutMe = () => {
         </h3>
 
         <p className="text-1xl text-white font-source-code-pro">
-          Innovative front-end developer with deep knowledge of React.js, HTML,
-          CSS, and JavaScript. Skilled in designing and implementing responsive
-          web applications with a focus on performance and user experience.
-          Experienced in utilizing Bootstrap and other frameworks to build
-          visually appealing interfaces. Currently enhancing backend development
-          skills to support a full-stack development approach and drive project
-          efficiency.
+          Innovative Full Stack developer with deep knowledge of React.js, HTML,
+          CSS, JavaScript, TypeScript and node js. Skilled in designing and
+          implementing responsive web applications with a focus on performance
+          and user experience. Experienced in utilizing Bootstrap and other
+          frameworks to build visually appealing interfaces. Currently enhancing
+          backend development skills to support a full-stack development
+          approach and drive project efficiency.
         </p>
       </div>
 
@@ -85,7 +87,7 @@ const AboutMe = () => {
       </div>
 
       <section className="lg:flex h-auto rounded-lg gap-20">
-        <div className="text-center shadow-lg p-10 my-10 bg-slate-500 bg-opacity-25 rounded-xl flex-1">
+        <div className="text-left shadow-lg p-10 my-10 bg-slate-500 bg-opacity-25 rounded-xl flex-1">
           <h1 className="text-3xl font-extrabold">Education</h1>
 
           <p className="text-1xl pt-3">
@@ -98,7 +100,7 @@ const AboutMe = () => {
           </p>
         </div>
 
-        <div className="text-center shadow-lg p-10 my-10 bg-slate-500 bg-opacity-25 rounded-xl flex-1">
+        <div className="text-left shadow-lg p-10 my-10 bg-slate-500 bg-opacity-25 rounded-xl flex-1">
           <h1 className="text-3xl font-extrabold">Language Skills</h1>
 
           <p className="text-1xl pt-3">Hindi - Native</p>
@@ -115,83 +117,92 @@ const AboutMe = () => {
         <div className="flex flex-wrap justify-center gap-16">
           <img
             src={HtmlIcon}
-            className="h-12 transition-transform duration-150 hover:scale-150 sm:h-12 md:h-14 lg:h-16 xl:h-20"
+            className="h-8 transition-transform duration-150 hover:scale-75 sm:h-10 md:h-12 lg:h-14 xl:h-16"
             alt="HTML Icon"
           />
           <img
             src={cssIcon}
-            className="h-12 transition-transform duration-150 hover:scale-150 sm:h-12 md:h-14 lg:h-16 xl:h-20"
+            className="h-12 transition-transform duration-150 hover:scale-125 sm:h-10 md:h-12 lg:h-14 xl:h-16"
             alt="CSS Icon"
           />
           <img
             src={javaIcon}
-            className="h-12 transition-transform duration-150 hover:scale-150 sm:h-12 md:h-14 lg:h-16 xl:h-20"
+            className="h-12 transition-transform duration-150 hover:scale-75 sm:h-10 md:h-12 lg:h-14 xl:h-16"
             alt="Java Icon"
           />
           <img
             src={reactIcon}
-            className="h-12 transition-transform duration-150 hover:scale-150 sm:h-12 md:h-14 lg:h-16 xl:h-20"
+            className="h-12 transition-transform duration-150 hover:scale-125 sm:h-10 md:h-12 lg:h-14 xl:h-16"
             alt="React Icon"
           />
           <img
             src={nextIcon}
-            className="h-12 transition-transform duration-150 hover:scale-150 sm:h-12 md:h-14 lg:h-16 xl:h-20"
+            className="h-12 transition-transform duration-150 hover:scale-75 sm:h-10 md:h-12 lg:h-14 xl:h-16"
             alt="Next.js Icon"
           />
         </div>
 
         <div className="flex flex-wrap justify-center gap-16 mt-8">
           <img
+            src={nodeIcon}
+            className="h-12 transition-transform duration-150 hover:scale-125 sm:h-10 md:h-12 lg:h-14 xl:h-16"
+            alt="node Icon"
+          />
+          <img
             src={bootStrapIcon}
-            className="h-12 transition-transform duration-150 hover:scale-150 sm:h-12 md:h-14 lg:h-16 xl:h-20"
+            className="h-12 transition-transform duration-150 hover:scale-75 sm:h-10 md:h-12 lg:h-14 xl:h-16"
             alt="Bootstrap Icon"
           />
           <img
             src={tailwindIcon}
-            className="h-12 transition-transform duration-150 hover:scale-150 sm:h-12 md:h-14 lg:h-16 xl:h-20"
+            className="h-12 transition-transform duration-150 hover:scale-125 sm:h-10 md:h-12 lg:h-14 xl:h-16"
             alt="Tailwind CSS Icon"
           />
           <img
             src={muiIcon}
-            className="h-12 transition-transform duration-150 hover:scale-150 sm:h-12 md:h-14 lg:h-16 xl:h-20"
+            className="h-12 transition-transform duration-150 hover:scale-75 sm:h-10 md:h-12 lg:h-14 xl:h-16"
             alt="MUI Icon"
           />
           <img
             src={envIcon}
-            className="h-12 transition-transform duration-150 hover:scale-150 sm:h-12 md:h-14 lg:h-16 xl:h-20"
+            className="h-12 transition-transform duration-150 hover:scale-125 sm:h-10 md:h-12 lg:h-14 xl:h-16"
             alt="Environment Icon"
           />
         </div>
       </section>
 
-      <section className="flex-col bg-slate-500 bg-opacity-25 p-5 mb-10 rounded-lg max-w-4xl mx-auto">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold text-center">
+      <section className="flex flex-col bg-slate-500 bg-opacity-25 p-8 mb-12 rounded-2xl max-w-5xl mx-auto shadow-lg backdrop-blur-sm">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-center text-white mb-8 tracking-wide">
           Technical Skills
         </h1>
 
-        <p className="mt-4 text-sm sm:text-base md:text-lg font-source-code-pro">
-          <strong>Frontend:</strong> React.js, HTML5, CSS3, JavaScript (ES6+),
-          TypeScript
-        </p>
+        <div className="space-y-6 text-gray-700 text-base sm:text-sm md:text-lg ">
+          <p className="text-sm text-white font-source-code-pro">
+            <strong className="text-blue-100">Frontend:</strong> React.js,
+            HTML5, CSS3, JavaScript (ES6+), TypeScript
+          </p>
 
-        <p className="mt-4 text-sm sm:text-base md:text-lg font-source-code-pro">
-          <strong>UI Frameworks:</strong> Bootstrap, Tailwind
-        </p>
+          <p className="text-sm text-white font-source-code-pro">
+            <strong className="text-blue-100">UI Frameworks:</strong> Bootstrap,
+            Tailwind
+          </p>
 
-        <p className="mt-4 text-sm sm:text-base md:text-lg font-source-code-pro">
-          <strong>State Management:</strong> Context API, Redux
-        </p>
+          <p className="text-sm text-white font-source-code-pro">
+            <strong className="text-blue-100">State Management:</strong> Context
+            API, Redux
+          </p>
 
-        <p className="mt-4 text-sm sm:text-base md:text-lg font-source-code-pro">
-          <strong>Language:</strong> Python
-        </p>
+          <p className="text-sm text-white font-source-code-pro">
+            <strong className="text-blue-100">Language:</strong> Python
+          </p>
 
-        <p className="mt-4 text-sm sm:text-base md:text-lg font-source-code-pro">
-          <strong>Project-specific Skills:</strong> Form Handling, State
-          Persistence, React Hooks, React Router, RESTful API Design, Signup and
-          Authentication, Real-Time Database, Custom Hooks, Cart Functionality,
-          Firebase (Backend)
-        </p>
+          <p className="text-sm text-white font-source-code-pro">
+            <strong className="text-blue-100">Project-specific Skills:</strong>{" "}
+            Form Handling, State Persistence, React Hooks, React Router, RESTful
+            API Design, Signup and Authentication, Real-Time Database, Custom
+            Hooks, Cart Functionality, Firebase (Backend)
+          </p>
+        </div>
       </section>
     </div>
   );
